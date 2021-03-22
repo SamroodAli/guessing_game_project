4b2e69e36793e818d883602f4b0ae27021bcce3a
samroodAli <devsamrood@gmail.com>
2021-03-22 18:21:33 +0530

    Initial commit

---
+++ lib/play_guessing_game.rb
@@ -0,0 +1,18 @@
+require_relative "guessing_game"
+
+print "Enter a min number: "
+min = gets.chomp.to_i
+
+print "Enter a max number: "
+max = gets.chomp.to_i
+
+puts "I'm thinking of a number between #{min} and #{max}"
+
+guessing_game = GuessingGame.new(min, max)
+
+until guessing_game.game_over?
+  guessing_game.ask_user
+  puts "---------------"
+end
+
+puts "You won in #{guessing_game.num_attempts} tries"
